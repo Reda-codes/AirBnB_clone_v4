43baa43e3b66184a49a788334ece7c50f3f822ed
Reda <muhammadredabh@gmail.com>
2023-01-29 13:19:45 +0100

    2. Select some Amenities to be comfortable!

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  let amenities = {};
+  $(document).on('change', "input[type='checkbox']", function () {
+    if (this.checked) {
+      amenities[$(this).data('id')] = $(this).data('name');
+      console.log(amenities)
+    } else {
+      delete amenities[$(this).data('id')];
+    }
+    let list = Object.values(amenities);
+    if (list.length > 0) {
+      $('div.amenities > h4').text(Object.values(amenities).join(', '));
+    } else {
+      $('div.amenities > h4').html('&nbsp;');
+    }
+  });
+});
