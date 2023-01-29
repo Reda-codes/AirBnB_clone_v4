2f93c122371ce7c8def41fd61714939054b9efd5
Reda <muhammadredabh@gmail.com>
2023-01-29 14:03:51 +0100

    3. API status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  let amenities = {};
+  $(document).on('change', "input[type='checkbox']", function () {
+    if (this.checked) {
+      amenities[$(this).data('id')] = $(this).data('name');
+      console.log(amenities)
+    } else {
+      delete amenities[$(this).data('id')];
+    }
+    let list = Object.values(amenities);
+    if (list.length > 0) {
+      $('div.amenities > h4').text(Object.values(amenities).join(', '));
+    } else {
+      $('div.amenities > h4').html('&nbsp;');
+    }
+  });
+  $.get('http://' + window.location.hostname + ':5001/api/v1/status', function (data, textStatus) {
+    if (textStatus === 'success') {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    }
+  });
+});
+
